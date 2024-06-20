1b6665cf63ee8869507b6f5f4968b24c6d4d628b
gnarus-g <37311893+Gnarus-G@users.noreply.github.com>
2024-06-20 19:13:28 -0400

    fix: use the restedlang tree-sitter repo

--- lua/restedlang/init.lua
+++ lua/restedlang/init.lua
@@ -2,12 +2,12 @@
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.restedlang = {
   install_info = {
-    url = "~/(d|projects)/tree-sitter-restedlang", -- local path or git repo
-    files = { "src/parser.c" },                    -- note that some parsers also require src/scanner.c or src/scanner.cc
+    url = "https://github.com/Gnarus-G/tree-sitter-restedlang", -- local path or git repo
+    files = { "src/parser.c" },                                 -- note that some parsers also require src/scanner.c or src/scanner.cc
     -- optional entries:
-    branch = "main",                               -- default branch in case of git repo if different from master
-    generate_requires_npm = false,                 -- if stand-alone parser without npm dependencies
-    requires_generate_from_grammar = false,        -- if folder contains pre-generated src/parser.c
+    branch = "main",                                            -- default branch in case of git repo if different from master
+    generate_requires_npm = false,                              -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false,                     -- if folder contains pre-generated src/parser.c
   },
 }
 
